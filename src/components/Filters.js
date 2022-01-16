cb71da238a33c6b4c46b75fd1f59149ac17faef4
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-16 13:11:29 -0500

    commit tree 04f93700568f7cb1b3b574fd152defee42debab6

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function Filters() {
+function Filters({onFindPetsClick, onChangeType}) {
+
+  function onChange(e){
+    const newFilter = e.target.value
+    onChangeType(newFilter)
+  }
+
+  function onClick(){
+    onFindPetsClick()
+  }
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select onChange={onChange} name="type" id="type" aria-label="type">
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
@@ -14,7 +24,7 @@ function Filters() {
       </div>
 
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button onClick={onClick} className="ui secondary button">Find pets</button>
       </div>
     </div>
   );
